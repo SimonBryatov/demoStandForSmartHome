7ff0917858a4c1c909d9ba3133900216fccd4a99
SimonBryatov <bsr-007@mail.ru>
2018-06-27 02:22:04 +0400

    wow

--- misc/serverOld.js
+++ misc/serverOld.js
@@ -1,20 +1,8 @@
 var SerialPort = require('serialport');
-
-var incommingData = new Buffer(0);
-var myParser = function(emitter, buffer) {
-    incommingData = Buffer.concat([incommingData, buffer]);
-    if (incommingData.length > 3 && incommingData[incommingData.length - 3] == 3) {
-        emitter.emit("data", incommingData);
-        incommingData = new Buffer(0);
-    }
-};
-
 var port = new SerialPort('COM3', {
   baudRate: 9600
 });
-
-const parser = port.pipe(new SerialPort.parsers.Delimiter({ delimiter: '\u0003' }))
-
+//port = port.pipe(new SerialPort.parsers.Delimiter({delimiter: "\u0003"}))
 const sSender = require("./socketSender")
 const comOut = require("./commandsOut")
 chalk = require("chalk");
@@ -31,11 +19,18 @@ http.listen(3000, function(){
   console.log('listening on *:3000');
 });
 
+let drainflag = 0;
+
+
+
+
+let buffer = "";
 port.on("open", () => {
   io.on('connection', function(socket){
     console.log('a user connected');
     socket.on("controlMsg", (data) => {
         let ind = comOut[data[0]]
+        let write = () => {
         if (ind) {
           port.write("\u0001" + ind + "\u0002" + Number(data[1]) + "\u0003", function(err) {
                     if (err) {
@@ -43,13 +38,27 @@ port.on("open", () => {
                     }
                     console.log(chalk.cyan("Data sent:"), "|", data[0], "|", data[1], "|", ind);
                   });  
+           //port.drain(() => {drainflag = 0; console.log('sended')})
         } else console.log(chalk.yellow(`No such output command! `) + data[0])
+      }
+    //  _.throttle(write, 1000)
+    write();
+    //  port.flush()
     })  
   });
 
-  parser.on('data', function (data) {
-    //console.log(data.toString())
-    sSender(data.toString());
+  port.on('data', function (data) {
+    buffer += data.toString()
+    console.log(buffer);
+    let del = buffer.indexOf(String.fromCharCode(3))
+    if (del >= 0) {
+    let arr = buffer.split("")
+    let res = arr.splice(0, del + 1); 
+    buffer = arr.join("")
+    //console.log(res.join(""))
+    sSender(res.join(""));
+    //port.flush()
+    }
   }); 
 
 let a = Math.random()*50;
